0ca880bc30929296022ed6917561713266ece050
jayeshjain4 <jayeshkumarjain4@gmail.com>
2025-06-25 11:39:33 +0530

    remove duplicate auth route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express';
-import authRoutes from './auth.routes';
 import postRoutes from './postRoutes';
 
 const router = Router();
 
-router.use('/auth', authRoutes);
+
 router.use('/post', postRoutes);
 
 export default router;
