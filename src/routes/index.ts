7c9ce5b6421ff7c9b4c181157cf49a023a43eac6
Jayeshkumar Jain <jayeshkumarjain4@gmail.com>
2025-09-01 23:44:31 +0530

    issue fixes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import authRoutes from './authRoutes';
+import categoryRoutes from './categoryRoutes';
 import postRoutes from './postRoutes';
 import likeRoutes from './likeRoutes';
 import commentRoutes from './commentRoutes';
@@ -6,8 +8,10 @@ import commentRoutes from './commentRoutes';
 const router = Router();
 
 // Register all routes
+router.use('/auth', authRoutes);
+router.use('/categories', categoryRoutes);
+router.use('/posts', postRoutes);
 router.use('/', likeRoutes);
 router.use('/', commentRoutes);
-router.use('/post', postRoutes);
 
 export default router;
